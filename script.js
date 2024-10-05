482676484fea2f326b1b28af313628a9e4172f0b
Jinling Han <hjl-990406@outlook.com>
2024-10-05 10:43:45 +1000

    add js to play and pause one music, except progress-bar

---
+++ script.js
@@ -0,0 +1,43 @@
+const music = document.getElementById('music');
+const playBtn = document.querySelector('.play-btn');
+const currTime = document.querySelector('.current-time');
+const ttlTime = document.querySelector('.total-time');
+const progressBar = document.querySelector('.progress-bar');
+const musicImg = document.querySelector('img');
+
+playBtn.addEventListener('click', () => {
+  if (playBtn.classList.contains('stop')) {
+    music.pause();
+    playBtn.classList.remove('stop');
+    playBtn.firstElementChild.innerHTML = 'play_arrow';
+    musicImg.style.animationPlayState = 'paused';
+  } else {
+    music.play();
+    playBtn.classList.add('stop');
+    playBtn.firstElementChild.innerHTML = 'pause';
+    musicImg.style.animationPlayState = 'running'
+  }
+});
+
+music.addEventListener('loadeddata', () => {
+  let audioDuration = music.duration;
+  let totalMin = Math.floor(audioDuration / 60);
+  let totalSec = Math.floor(audioDuration % 60);
+  if (totalSec < 10) {
+    totalSec = `0${totalSec}`;
+  }
+  ttlTime.innerHTML = `${totalMin}:${totalSec}`;
+})
+
+music.addEventListener('timeupdate', (e) => {
+  let currentTime = e.target.currentTime;
+  let audioDuration = e.target.duration;
+  let currMin = Math.floor(currentTime / 60);
+  let currSec = Math.floor(currentTime % 60);
+  if (currSec < 10) {
+    currSec = `0${currSec}`;
+  }
+  currTime.innerHTML = `${currMin}:${currSec}`;
+  let progressWidth = (currentTime / audioDuration) * 100;
+  progressBar.style.width = `${progressWidth}%`;
+});
